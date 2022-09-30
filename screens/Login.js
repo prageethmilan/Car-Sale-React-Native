fcc1dfaf3de29fbb38976048f29674136b41e1d3
prageethmilan <prageethmilan1999@gmail.com>
2022-09-30 18:50:19 +0530

    add background Image

--- screens/Login.js
+++ screens/Login.js
@@ -1,32 +1,28 @@
-import React from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { View, Text, StyleSheet, Dimensions, ImageBackground } from 'react-native'
+import React from 'react'
 
-const image = { uri: "https://reactjs.org/logo-og.png" };
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+const image = require('../assets/login_background.jpg')
 
-const Login = () => (
-  <View style={styles.container}>
-    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-      <Text style={styles.text}>Inside</Text>
-    </ImageBackground>
-  </View>
-);
+export default function Login() {
+
+    return (
+        <View style={styles.container}>
+            <ImageBackground source={image} resizeMode='cover' style={styles.img}>
+                
+            </ImageBackground>
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    width:'100%',
-    height:'100%'
-  },
-  text: {
-    color: "white",
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: "bold",
-    textAlign: "center",
-    backgroundColor: "#000000c0"
-  }
-});
-
-export default Login;
+    container: {
+        width: windowWidth,
+        height: windowHeight
+    },
+    img:{
+        width:'100%',
+        height:'100%'
+    }
+})
