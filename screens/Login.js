b380c48b2f0e8e355e484205807a637f4336f12c
prageethmilan <prageethmilan1999@gmail.com>
2022-10-01 10:37:15 +0530

    Login Screen Complete

--- screens/Login.js
+++ screens/Login.js
@@ -1,6 +1,6 @@
-import { View, Text, StyleSheet, Dimensions, ImageBackground,Image } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, ImageBackground, Image } from 'react-native'
 import React from 'react'
-import { NativeBaseProvider, Box } from 'native-base'
+import { NativeBaseProvider, Box, Input, FormControl, Stack, Button } from 'native-base'
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -14,7 +14,22 @@ export default function Login() {
         <NativeBaseProvider>
             <Box style={styles.container}>
                 <ImageBackground source={image} resizeMode='cover' style={styles.img}>
-                <Image source={require('../assets/logo.png')} style={styles.logo}/>
+                    <Image source={require('../assets/logo.png')} style={styles.logo} />
+                    <FormControl isRequired>
+                        <Stack mx="4">
+                            <FormControl.Label>Username</FormControl.Label>
+                            <Input type="text" style={styles.input} require />
+                            <FormControl.Label>Password</FormControl.Label>
+                            <Input type="password" style={styles.input} require />
+                            <Button size="md" variant="subtle" colorScheme="purple" style={styles.login_btn}>
+                                <Text style={styles.login_btn_label}>Login</Text>
+                            </Button>
+                        </Stack>
+                    </FormControl>
+                    <Text style={styles.signup_label}>Don't have an account?</Text>
+                    <Button size="md" variant="link" colorScheme={'secondary'} style={styles.btn_Signup}>
+                        <Text style={styles.btn_Signup_label}>Sign Up</Text>
+                    </Button>
                 </ImageBackground>
             </Box>
         </NativeBaseProvider>
@@ -30,10 +45,40 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%'
     },
-    logo:{
-        width:'60%',
-        height:'20%',
-        marginTop:'30%',
-        alignSelf:'center'
+    logo: {
+        width: '60%',
+        height: '20%',
+        marginTop: '30%',
+        alignSelf: 'center'
+    },
+    input: {
+        color: 'white',
+        fontSize: 20
+    },
+    login_btn: {
+        marginTop: '4%'
+    },
+    login_btn_label: {
+        color: 'purple',
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
+    signup_label: {
+        position: 'absolute',
+        color: 'white',
+        fontSize: 17,
+        bottom: '4%',
+        left: '15%'
+    },
+    btn_Signup:{
+        width:'30%',
+        position:'absolute',
+        bottom:'2.5%',
+        left:'55%'
+    },
+    btn_Signup_label:{
+        color:'pink',
+        fontWeight:'bold',
+        fontSize:17
     }
 })
